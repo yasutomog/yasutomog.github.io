22afd1a0804c31136d98b416ec7acdf946b73011
yasutomog <yasutomo.goto@gmail.com>
2018-10-15 20:17:06 +0900

    change test pwa

--- sw.js
+++ sw.js
@@ -8,24 +8,24 @@ workbox.precaching.precacheAndRoute([
     "revision": "e504db990acf994053e2582efbdc2e27"
   },
   {
-    "url": "/_nuxt/6ae5e68974d5af3762e2.js",
-    "revision": "ecad8b3e69d02bfc02f2c65552828092"
+    "url": "/_nuxt/7e97ce78d138cc93e648.js",
+    "revision": "ef76795bbcce1af063ff95babcc8e327"
   },
   {
-    "url": "/_nuxt/a4e56623652754863dcf.js",
-    "revision": "a133598435dc2b6f6b7708fcb3e9dbb2"
+    "url": "/_nuxt/ac277505c44154d12af5.js",
+    "revision": "85dc6bfda15b72a100dda6f6d5127ebb"
   },
   {
     "url": "/_nuxt/ae30404b63671f56892b.js",
     "revision": "9316f3506616c3aebad0dfb82d07ee7c"
   },
   {
-    "url": "/_nuxt/e086b003a5a834c0e0c5.js",
-    "revision": "3cb1f6507dd7e73e6e1a6452789dab85"
+    "url": "/_nuxt/b686d23d7e503c94f284.js",
+    "revision": "ab0599e742ed37c3aaa1aef5358c299b"
   },
   {
-    "url": "/_nuxt/fd597d14fe5b6489d0cd.js",
-    "revision": "902182fdd9df33a98c519bfdf3abddf3"
+    "url": "/_nuxt/e086b003a5a834c0e0c5.js",
+    "revision": "3cb1f6507dd7e73e6e1a6452789dab85"
   }
 ], {
   "cacheId": "book-management-nuxt-pwa",
